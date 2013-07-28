864474d72ff6a0718ec420326d54365a423c6607
kalle <kalle@kodapan.se>
2013-07-28 21:16:06 +0200

    more logging

--- src/main/java/se/kodapan/osm/services/nominatim/AbstractCachedNominatim.java
+++ src/main/java/se/kodapan/osm/services/nominatim/AbstractCachedNominatim.java
@@ -1,17 +1,25 @@
 package se.kodapan.osm.services.nominatim;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author kalle
  * @since 2013-07-28 19:57
  */
 public abstract class AbstractCachedNominatim extends Nominatim {
 
+  private static Logger log = LoggerFactory.getLogger(AbstractCachedNominatim.class);
+
   @Override
   public synchronized String search(String url) throws Exception {
     String response = getCachedResponse(url);
     if (response == null) {
+      log.info("No cached response available, sending query to super.");
       response = super.search(url);
       setCachedResponse(url, response);
+    } else if (log.isDebugEnabled()) {
+      log.debug("Cached response available.");
     }
     return response;
 
--- src/main/java/se/kodapan/osm/services/overpass/AbstractCachedOverpass.java
+++ src/main/java/se/kodapan/osm/services/overpass/AbstractCachedOverpass.java
@@ -1,18 +1,27 @@
 package se.kodapan.osm.services.overpass;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author kalle
  * @since 2013-07-27 23:53
  */
 public abstract class AbstractCachedOverpass extends Overpass {
 
+  private static Logger log = LoggerFactory.getLogger(AbstractCachedOverpass.class);
+
   @Override
   public String execute(String overpassQuery, String queryDescription) throws Exception {
     String response = getCachedResponse(overpassQuery);
     if (response == null) {
+      log.info("No cached response available, sending query to super.");
       response = super.execute(overpassQuery, queryDescription);
       setCachedResponse(overpassQuery, response);
+    } else if (log.isDebugEnabled()) {
+      log.debug("Cached response available.");
     }
+
     return response;
   }
 
