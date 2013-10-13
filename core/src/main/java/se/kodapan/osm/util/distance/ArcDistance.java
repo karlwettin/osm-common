efe2e1c2adeaf3cd3837e3fc1b37d6d0c94d2293
kalle <kalle@kodapan.se>
2013-10-13 04:05:31 +0200

    new implementation

--- core/src/main/java/se/kodapan/osm/util/distance/ArcDistance.java
+++ core/src/main/java/se/kodapan/osm/util/distance/ArcDistance.java
@@ -21,64 +21,19 @@ public class ArcDistance  extends Distance {
   @Override
   public double calculate(double latitudeA, double longitudeA, double latitudeB, double longitudeB) {
 
-    longitudeA = normalizeLongitude(longitudeA);
-    longitudeB = normalizeLongitude(longitudeB);
 
-    // Check for same position
-    if (latitudeA == latitudeB && longitudeA == longitudeB)
-      return 0.0;
+    double dLatitude = Math.toRadians(latitudeB-latitudeA);
+    double dLongitude = Math.toRadians(longitudeB-longitudeA);
+    double a = Math.sin(dLatitude/2) * Math.sin(dLatitude/2) +
+        Math.cos(Math.toRadians(latitudeA)) * Math.cos(Math.toRadians(latitudeB)) *
+            Math.sin(dLongitude/2) * Math.sin(dLongitude/2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-    // Get the m_dLongitude diffeernce. Don't need to worry about
-    // crossing 180 since cos(x) = cos(-x)
-    double dLon = longitudeB - longitudeA;
+    double radiusEarth = 6371; // km
+    double distance = radiusEarth * c;
 
-    double a = radians(90.0 - latitudeA);
-    double c = radians(90.0 - latitudeB);
-    double cosB = (Math.cos(a) * Math.cos(c))
-        + (Math.sin(a) * Math.sin(c) * Math.cos(radians(dLon)));
-
-    //    double radius = (lUnits == DistanceUnits.MILES) ? 3963.205/* MILERADIUSOFEARTH */
-    //        : 6378.160187/* KMRADIUSOFEARTH */;
-
-    double radius = 6378.160187;
-
-    // Find angle subtended (with some bounds checking) in radians and
-    // multiply by earth radius to find the arc distance
-    if (cosB < -1.0)
-      return 3.14159265358979323846/* PI */ * radius;
-    else if (cosB >= 1.0)
-      return 0;
-    else
-      return Math.acos(cosB) * radius;
+    return distance;
   }
 
-  /**
-   * copied from org.apache.lucene.spatial.base.LatLng
-   *
-   * @param a
-   * @return
-   */
-  private double radians(double a) {
-    return a * 0.01745329251994;
-  }
 
-  /**
-   * copied from org.apache.lucene.spatial.base.LatLng
-   *
-   * @param longitude
-   * @return
-   */
-  private double normalizeLongitude(double longitude) {
-    double delta = 0;
-    if (longitude < 0) delta = 360;
-    if (longitude >= 0) delta = -360;
-
-    double normalizedLongitude = longitude;
-    while (normalizedLongitude <= -180 || normalizedLongitude >= 180) {
-      normalizedLongitude += delta;
-    }
-
-    return normalizedLongitude;
-  }
-
-  }
+}
