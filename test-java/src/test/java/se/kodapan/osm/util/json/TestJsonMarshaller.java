7cbede7cb3d07466680bf8fd1c04362282dffaeb
kalle <karl.wettin@gmail.com>
2013-12-31 02:13:23 +0100

    new module tools for tile server utilities, etc that use the core features.

--- test-java/src/test/java/se/kodapan/osm/util/json/TestJsonMarshaller.java
+++ test-java/src/test/java/se/kodapan/osm/util/json/TestJsonMarshaller.java
@@ -1,8 +1,44 @@
 package se.kodapan.osm.util.json;
 
+import junit.framework.TestCase;
+import se.kodapan.osm.domain.Node;
+import se.kodapan.osm.domain.root.PojoRoot;
+import se.kodapan.osm.domain.root.Root;
+import se.kodapan.osm.parser.xml.instantiated.InstantiatedOsmXmlParser;
+
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+
 /**
  * @author kalle
  * @since 2013-11-02 14:18
  */
-public class TestJsonMarshaller {
+public class TestJsonMarshaller extends TestCase {
+
+  public void test() throws Exception {
+
+    JsonMarshaller marshaller = new JsonMarshaller();
+    JsonUnmarshaller unmarshaller = new JsonUnmarshaller();
+
+    PojoRoot root = new PojoRoot();
+    InstantiatedOsmXmlParser parser = InstantiatedOsmXmlParser.newInstance();
+    parser.setRoot(root);
+
+    parser.parse(new InputStreamReader(getClass().getResourceAsStream("/fjallbacka.osm.xml"), "UTF8"));
+
+    Root.Enumerator<Node> nodes = root.enumerateNodes();
+    Node node;
+    while ((node = nodes.next()) != null) {
+
+      StringWriter marshalled = new StringWriter();
+      marshaller.serialize(node, marshalled);
+
+//      Node unmarshalled = unmarshaller.deserializeNode(marshalled.toString());
+
+      System.currentTimeMillis();
+
+    }
+
+  }
+
 }
