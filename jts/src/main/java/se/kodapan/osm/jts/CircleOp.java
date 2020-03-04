350bda56644a7943366e98bbdbe8bc68a4d86dc6
Karl Wettin <karl.wettin@wikimedia.se>
2020-03-04 01:05:46 +0100

    Creates a polygon in the shape of a circle

---
+++ jts/src/main/java/se/kodapan/osm/jts/CircleOp.java
@@ -0,0 +1,51 @@
+package se.kodapan.osm.jts;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import se.kodapan.osm.domain.Node;
+
+public class CircleOp {
+
+  public CircleOp() {
+    this(new GeometryFactory());
+  }
+
+  private GeometryFactory geometryFactory;
+
+  public CircleOp(GeometryFactory geometryFactory) {
+    this.geometryFactory = geometryFactory;
+  }
+
+  public Polygon circle(Point point, double radiusKilometers, int circumferenceResolution) {
+    return circle(point.getCoordinate(), radiusKilometers, circumferenceResolution);
+  }
+
+  public Polygon circle(Coordinate coordinate, double radiusKilometers, int circumferenceResolution) {
+    return circle(coordinate.x, coordinate.y, radiusKilometers, circumferenceResolution);
+  }
+
+  public Polygon circle(Node node, double radiusKilometers, int circumferenceResolution) {
+    return circle(node.getLongitude(), node.getLatitude(), radiusKilometers, circumferenceResolution);
+  }
+
+  public Polygon circle(double centroidLongitude, double centroidLatitude, double radiusKilometers, int circumferenceResolution) {
+    Coordinate[] coordinates = new Coordinate[circumferenceResolution + 1];
+
+    double radiusLatitude = (radiusKilometers / 6378.8d) * (180 / Math.PI);
+    double radiusLongitude = radiusLatitude / Math.cos(centroidLatitude * (Math.PI / 180));
+
+    int index = 0;
+    int step = (int) (360d / (double) circumferenceResolution);
+    for (int i = 0; i < 360; i += step) {
+      double a = i * (Math.PI / 180);
+      double latitude = centroidLatitude + (radiusLatitude * Math.sin(a));
+      double longitude = centroidLongitude + (radiusLongitude * Math.cos(a));
+      coordinates[index++] = new Coordinate(longitude, latitude);
+    }
+    coordinates[coordinates.length - 1] = coordinates[0];
+
+    return geometryFactory.createPolygon(geometryFactory.createLinearRing(coordinates), null);
+  }
+}
